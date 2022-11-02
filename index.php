358ea1ba8475b74f8f17082fece0d0b5c2ec9f42
curcur4 <95090174+curcur4@users.noreply.github.com>
2022-11-02 20:39:57 +0300

    solved log in problem

--- index.php
+++ index.php
@@ -37,7 +37,7 @@ if (isset($_POST['login'])) {
 	$email = mysqli_real_escape_string($conn, $_POST['email']);
 	$pass = md5($_POST['password']);
 
-	$select = " SELECT * FROM login WHERE login_email = '$email' && login_rank = '$pass'";
+	$select = " SELECT * FROM login WHERE login_email = '$email' && login_password = '$pass'";
 
 	$result = mysqli_query($conn, $select);
 
@@ -67,83 +67,84 @@ if (isset($_POST['login'])) {
 <html lang="en">
 
 <head>
-	<meta charset="UTF-8">
-	<meta http-equiv="X-UA-Compatible" content="IE=edge">
-	<meta name="viewport" content="width=device-width, initial-scale=1.0">
-	<title>Farm&Cust | login</title>
-	<link rel="stylesheet" href="css/style.css">
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Farm&Cust | login</title>
+    <link rel="stylesheet" href="css/style.css">
 </head>
 
 <body>
 
-	<nav class="nav">
-		<div class="logo">FARM&CUST </div>
-	</nav>
+    <nav class="nav">
+        <div class="logo">FARM&CUST </div>
+    </nav>
 
-	<main>
-		<div class="container">
-			<div class="card">
-				<div class="inner-box" id="card">
-					<div class="card-front">
-						<form action="" method="post">
-							<h1 class="title">Login</h1>
-							<input type="email" name="email" class="input" placeholder="a" required>
-							<label for="" class="label">Email</label>
+    <main>
+        <div class="container">
+            <div class="card">
+                <div class="inner-box" id="card">
+                    <div class="card-front">
+                        <form action="" method="post">
+                            <h1 class="title">Login</h1>
+                            <input type="email" name="email" class="input" placeholder="a" required>
+                            <label for="" class="label">Email</label>
 
-							<input type="password" name="password" class="input" placeholder="a" required>
-							<label for="" class="label">Password</label>
+                            <input type="password" name="password" class="input" placeholder="a" required>
+                            <label for="" class="label">Password</label>
 
-							<input type="submit" name="login" class="submitBtn" value="Login">
-							<button type="button" class="btn" placeholde="a" onclick="openSignup()">I'm new
-								here</button>
-							<a href="">Forgot password</a>
-						</form>
-					</div>
-					<div class="card-back">
-						<form action="" method="post">
-							<h1 class="title">Signup</h1>
-							<input type="email" name="email" class="input" placeholder="a">
-							<label for="" class="label">Email</label>
-							<fieldset>
-								<legend>Choose user type </legend>
-								<label for="user_role"></label>
-								<select name="user_role">
-									<option value="farmer">Farmer</option>
-									<option value="customer">Customer</option>
-									<option value="admin">Admin</option>
-								</select>
-							</fieldset>
-							<input type="password" name="password" class="input" placeholder="a">
-							<label for="password" class="label">Password</label>
-							<input type="password" name="cpassword" class="input" placeholder="a">
-							<label for="cpassword" class="label">Confirm Password</label>
-							<input type="submit" name="signup" class="submitBtn" value="Signup">
-							<button type="button" class="btn" placeholde="a" onclick="openLogin()">I have an account</button>
-						</form>
-					</div>
-				</div>
-				<?php
+                            <input type="submit" name="login" class="submitBtn" value="Login">
+                            <button type="button" class="btn" placeholde="a" onclick="openSignup()">I'm new
+                                here</button>
+                            <a href="">Forgot password</a>
+                        </form>
+                    </div>
+                    <div class="card-back">
+                        <form action="" method="post">
+                            <h1 class="title">Signup</h1>
+                            <input type="email" name="email" class="input" placeholder="a">
+                            <label for="" class="label">Email</label>
+                            <fieldset>
+                                <legend>Choose user type </legend>
+                                <label for="user_role"></label>
+                                <select name="user_role">
+                                    <option value="farmer">Farmer</option>
+                                    <option value="customer">Customer</option>
+                                    <option value="admin">Admin</option>
+                                </select>
+                            </fieldset>
+                            <input type="password" name="password" class="input" placeholder="a">
+                            <label for="password" class="label">Password</label>
+                            <input type="password" name="cpassword" class="input" placeholder="a">
+                            <label for="cpassword" class="label">Confirm Password</label>
+                            <input type="submit" name="signup" class="submitBtn" value="Signup">
+                            <button type="button" class="btn" placeholde="a" onclick="openLogin()">I have an
+                                account</button>
+                        </form>
+                    </div>
+                </div>
+                <?php
 				if (isset($error)) {
 					foreach ($error as $error) {
 						echo '<span class="error-msg">' . $error . '</span>';
 					};
 				};
 				?>
-			</div>
-		</div>
-	</main>
-	<footer></footer>
-	<script>
-		var card = document.getElementById("card");
+            </div>
+        </div>
+    </main>
+    <footer></footer>
+    <script>
+    var card = document.getElementById("card");
 
-		function openSignup() {
-			card.style.transform = "rotateY(-180deg)";
-		}
+    function openSignup() {
+        card.style.transform = "rotateY(-180deg)";
+    }
 
-		function openLogin() {
-			card.style.transform = "rotateY(0deg)";
-		}
-	</script>
+    function openLogin() {
+        card.style.transform = "rotateY(0deg)";
+    }
+    </script>
 </body>
 
 </html>
