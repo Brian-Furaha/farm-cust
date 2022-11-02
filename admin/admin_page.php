82c32b8fe8ae0cc6a5ad4c6f8e4168e9a20d0406
curcur4 <95090174+curcur4@users.noreply.github.com>
2022-11-02 20:23:59 +0300

    change in writing the path

--- admin/admin_page.php
+++ admin/admin_page.php
@@ -1,24 +1,24 @@
-<!DOCTYPE html>
-<html lang="en">
+    <!DOCTYPE html>
+    <html lang="en">
 
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Admin Dashboard</title>
-    <link rel="stylesheet" href="../css/admin.css">
-</head>
+    <head>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Admin Dashboard</title>
+        <link rel="stylesheet" href="../css/admin.css">
+    </head>
 
-<body>
-    <div class="container">
-        <div class="content">
-            <h3>hi, <span>admin</span></h3>
-            <h1>welcome <span></span></h1>
-            <p>this is an admin page</p>
-            <a href="../index.php" class="btn">login or register</a>
-            <a href="../logout.php" class="btn">Logout</a>
+    <body>
+        <div class="container">
+            <div class="content">
+                <h3>hi, <span>admin</span></h3>
+                <h1>welcome <span></span></h1>
+                <p>this is an admin page</p>
+                <a href="../index.php" class="btn">login or register</a>
+                <a href="../logout.php" class="btn">Logout</a>
+            </div>
         </div>
-    </div>
-</body>
+    </body>
 
-</html>
+    </html>
