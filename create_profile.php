825d68736e58a51fea237be9b13948d4d08c5914
Brian Furaha <95090174+Brian-Furaha@users.noreply.github.com>
2022-11-03 18:50:40 +0300

    create profile

---
+++ create_profile.php
@@ -0,0 +1,35 @@
+<?php
+
+ob_start();
+
+@include 'db/conn.php';
+
+session_start();
+
+
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Create profile</title>
+    <link rel="stylesheet" href="css/style.css">
+</head>
+<body>
+    <div class="profile">
+        <div class="flex">
+            <div class="inputbox">
+                <span>Enter your full name:</span>
+                <span>Enter your location</span>
+                <span></span>
+                <span></span>
+                <span></span>
+            </div>
+        </div>
+    </div>
+</body>
+</html>
